df2d78a63ef0b93350ca4b34c029700bfe17d954
nicolaazonta <nicolazonta114@gmail.com>
2023-03-09 17:32:18 +0100

    insert inputs and button in html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,16 @@
 
 
-
 //input-> chiedere al passeggero il numero di km da percorrere
+//passengerKm= document.getElementById('button');
+console.log(document.getElementById('name_surname'));
+
+
+
 //input-> chiedere al passeggero l'età
     //se >65 sconto del 40%
     //se <18 sconto del 20%
     //per tutti il prezzo al km è di 0.21€
+
 
 
 
